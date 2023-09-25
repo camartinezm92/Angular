d9eaa910a417c8e7cc83fe52f88c97b227445d80
Your NameCamiloMartinez <camartinezm92@gmail.com>
2023-09-25 17:30:04 -0500

    heroesapp

---
+++ 06-HeroesApp/src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AuthRoutingModule } from './auth-pouting.module';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
+import { RegisterPagesComponent } from './pages/register-pages/register-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutPagesComponent,
+    LoginPagesComponent,
+    RegisterPagesComponent,
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+
+  ]
+})
+export class AuthModule { }
---
+++ 06-HeroesApp/src/app/auth/pages/layout-pages/layout-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pages-layout-pages',
+  templateUrl: './layout-pages.component.html',
+  styles: [
+  ]
+})
+export class LayoutPagesComponent {
+
+}
