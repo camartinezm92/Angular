2f9a96120fc23304e6c1b08d32602fe0950fe2f1
Your NameCamiloMartinez <camartinezm92@gmail.com>
2023-09-30 12:51:38 -0500

    lazyload

--- 06-HeroesApp/src/app/auth/auth.module.ts
+++ 06-HeroesApp/src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { AuthRoutingModule } from './auth-pouting.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
--- 06-HeroesApp/src/app/shared/shared.module.ts
+++ 06-HeroesApp/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Error404pageComponent } from './pages/error404page/error404page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Error404pageComponent
+  ],
   imports: [
-    CommonModule
+  ],
+  exports:[
+    Error404pageComponent
   ]
 })
 export class SharedModule { }
