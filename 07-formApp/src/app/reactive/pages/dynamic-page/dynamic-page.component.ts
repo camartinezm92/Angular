6b158bb61be3b037b7392c299f3d2d93f3066968
Your NameCamiloMartinez <camartinezm92@gmail.com>
2023-11-25 15:28:06 -0500

    dynamic form

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+
 })
 export class DynamicPageComponent {
+
+  public  myFor2: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear',[Validators.required]],
+      ['DeathPool',[Validators.required]],
+    ])
+  })
+
+
+  constructor( private fb: FormBuilder){}
+
+  onSubmit():void{
+      if(this.myFor2.invalid){
+        this.myFor2.markAllAsTouched();
+        return;
+      }
+
+      console.log(this.myFor2.value);
+
+  }
 
 }
